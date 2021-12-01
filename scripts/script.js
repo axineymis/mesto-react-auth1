0bac590d223bb70fc6aab727e63272a382846912
axineymis <otm-natasha.biryukova@yandex.ru>
2021-12-01 23:55:02 +0300

    fix cards

--- scripts/script.js
+++ scripts/script.js
@@ -9,6 +9,67 @@ let profileText = document.querySelector('.profile__text');
 const editButton = document.querySelector('.profile__edit-button');
 const addButton = document.querySelector('.profile__add');
 
+const cardElements = document.querySelector(".elements");
+const templateEl = document.querySelector(".template__card");
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+  function render() {
+    const html = initialCards
+      .map((item, idx, arr) => {
+        return getItem(item);
+      });
+  
+      cardElements.append(...html);
+  }
+  
+
+function getItem(item) {
+    const newItem = templateEl.content.cloneNode(true);
+    const cardTitle = newItem.querySelector('.element__title');
+    const cardPhoto = newItem.querySelector('.element__photo');
+    const cardLikeBtn = newItem.querySelector('.element__like-button');
+    const deleteBtn = newItem.querySelector('.element__delete-btn');
+    cardTitle.textContent = item.name;
+    cardPhoto.src = item.link;
+    cardPhoto.alt = item.name;
+   
+    return newItem;
+};
+
+render();
+
+
+
+
+
+
+
+
 function openPopup() {
     nameInput.value = profileName.textContent;
     textInput.value = profileText.textContent;
