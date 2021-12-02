b30312fe2a134fcaf60321b9871d19f68d7ad46c
axineymis <otm-natasha.biryukova@yandex.ru>
2021-12-02 04:53:00 +0300

    fix close icone

--- scripts/script.js
+++ scripts/script.js
@@ -8,7 +8,16 @@ let profileName = document.querySelector('.profile__name');
 let profileText = document.querySelector('.profile__text');
 const editButton = document.querySelector('.profile__edit-button');
 const addButton = document.querySelector('.profile__add');
+ 
+const popupProfile = document.querySelector(".popup_type_edit");
+const popupAdd = document.querySelector(".popup_type_add");
+const popupClose = document.querySelector(".popup_type_add");
 
+let titleImput = formContent.querySelector('.popup__input_type_title');
+let imgImput = formContent.querySelector('.popup__input_type_img');
+let elementTitle = document.querySelector('.element__title');
+let elementImg = document.querySelector('.element__photo');
+const saveImgBtn = document.querySelector('.popup_add-button');
 const cardElements = document.querySelector(".elements");
 const templateEl = document.querySelector(".template__card");
 const initialCards = [
@@ -57,36 +66,50 @@ function getItem(item) {
     cardTitle.textContent = item.name;
     cardPhoto.src = item.link;
     cardPhoto.alt = item.name;
-   
+    deleteBtn.addEventListener('click', handDelete);
+
     return newItem;
 };
 
 render();
 
+function handDelete(evt) {
+    const targetCard = evt.target;
+    const listItem = targetCard.closest(".element");
+    listItem.remove();
+  }
 
-
-
-
-
-
-
-function openPopup() {
+function openPopupEdit() {
     nameInput.value = profileName.textContent;
     textInput.value = profileText.textContent;
-    formContent.classList.add("popup_open")
+    popupProfile.classList.add("popup_open")
 }
 
-function closePopup() {
-    formContent.classList.remove("popup_open")
+function closePopupEdit() {
+    popupProfile.classList.remove("popup_open")
 }
 
-editButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
+editButton.addEventListener('click', openPopupEdit);
+closeButton.addEventListener('click', closePopupEdit);
 formElement.addEventListener('submit', formSubmitHandler);
+
+function openPopupAdd() {
+    popupAdd.classList.add("popup_open")
+}
+
+function closePopupAdd() {
+    popupAdd.classList.remove("popup_open")
+}
+
+addButton.addEventListener('click', openPopupAdd);
+popupClose.addEventListener('click', closePopupAdd);
 
 function formSubmitHandler (evt) {
     evt.preventDefault(); 
     profileName.textContent = nameInput.value
     profileText.textContent = textInput.value
-    closePopup();
+    closePopupEdit();
 }
+
+
+
