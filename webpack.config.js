8f1e629328ecf2a7acfc4886aff8beb4bb784cae
axineymis <otm-natasha.biryukova@yandex.ru>
2022-01-30 11:10:51 +0300

    fix with comments

--- webpack.config.js
+++ webpack.config.js
@@ -35,8 +35,11 @@ module.exports = {
         // при обработке этих файлов нужно использовать
         // MiniCssExtractPlugin.loader и css-loader
         use: [MiniCssExtractPlugin.loader, {
-          loader: 'css-loader'
-        }]
+            loader: 'css-loader',
+            // добавьте объект options
+            options: { importLoaders: 1 }
+        },
+        'postcss-loader']
       }
     ]
   },
