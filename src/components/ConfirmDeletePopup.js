fe39d371ead46e13957ca45235650b9f1e2c97e1
axineymis <otm-natasha.biryukova@yandex.ru>
2022-04-04 02:27:11 +0300

    fix index.js

---
+++ src/components/ConfirmDeletePopup.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+function ConfirmDeletePopup(props) {
+    function handleSubmit(e) {
+        e.preventDefault();
+        props.handleCardDelete();
+        props.onClose();
+      }
+    return (
+        <div className={props.isOpen
+            ? `popup popup_type_${props.name} popup_opened`
+            : `popup popup_type_${props.name}`} >
+            <div className="popup__overlay" onClick={props.onClose}></div>
+            <div className="popup__content">
+                <button className="popup__close-button" onClick={props.onClose} type="button" ></button>
+                <h2 className="popup__title">{props.title}</h2>
+                <form className="popup__content-form" name={`${props.name}`} onSubmit={handleSubmit}>
+                    {props.children}
+                    <button className="popup__button" type="submit"> {props.buttonText}
+                    </button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default ConfirmDeletePopup;
